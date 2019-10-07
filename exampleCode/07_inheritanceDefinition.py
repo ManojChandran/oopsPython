781a161b8baa4602bd1ca302e272e6179d39699e
ManojChandran <manoj.meparmbu@gmail.com>
2019-10-07 12:16:11 -0400

    added inheritance example

---
+++ exampleCode/07_inheritanceDefinition.py
@@ -0,0 +1,21 @@
+# Inheritance example.
+# Pass class value instead of object while defining.
+class Mammal(object):
+    """docstring for Mamal."""
+
+    def is_a_mammal(self):
+        print('is a mammal')
+
+class Whale(Mammal): # Pass class and getting inheritted
+    """docstring for Whale."""
+
+    def who_is_this(self):
+        print('Whale')
+
+def main():
+    w = Whale() # instantiating Object whale
+    w.who_is_this()
+    w.is_a_mammal() # inherited is a relationship
+
+if __name__ == '__main__':
+    main()
