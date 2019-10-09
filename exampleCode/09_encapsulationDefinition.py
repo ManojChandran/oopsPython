becf25170235a925870ca9d6969ac8311884626e
ManojChandran <manoj.meparmbu@gmail.com>
2019-10-09 12:51:52 -0400

    adjusted examples

---
+++ exampleCode/09_encapsulationDefinition.py
@@ -0,0 +1,22 @@
+# defining Encapsulation
+class ProductEncapsulation(object):
+    """docstring ProdEncapsulation."""
+
+    def __init__(self):
+        super().__init__()
+        self.__productName = 't-shirt'
+        self.__brandCode = '871628736777'
+
+    def getProductname(self):
+        return self.__productName
+
+    def getbradCode(self):
+        return self.__brandCode
+
+def main():
+    Item = ProductEncapsulation()
+    print(Item.getProductname())
+    print(Item.getbradCode())
+
+if __name__ == '__main__':
+    main()
